a642986346bcff47b269f2ee36de97251e28b2a8
CyberJunnkie <abdullahyasin3295@gmail.com>
2021-07-25 16:56:10 -0400

    New CTF added

---
+++ DriftingBlue1/stuff/password.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+p = "1mw4ckyyucky"
+i = 0
+while i <= 100:
+	print (p+str(i))
+	i +=1
+
---
+++ DriftingBlue1/stuff/password.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+p="1mw4ckyyucky"
+i=0
+while [ $i -le 10]
+do
+output=$p$i
+i= i+1
+echo $output
+done
+
